5811c8c9c7c342b76c21e6c2a9212eef203419e4
Latthasay Saisanavong <Latthasay@gmail.com>
2025-05-22 15:38:13 +1000

    updated models.py to include usr class

--- project/models.py
+++ project/models.py
@@ -1,8 +1,12 @@
 from . import db
+#import werkzeug securtiy to  hash passwords
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
+    #added a user ID to link restaurant and user
+    userID = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     @property
     def serialize(self):
@@ -31,4 +35,24 @@ class MenuItem(db.Model):
            'price'      : self.price,
            'course'     : self.course,
        }
+    
+    #new user model
+class User(db.Model):
+    #user details
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    isAdmin = db.Column(db.Boolean, default=False)
+
+    restaurant = db.relationship(Restaurant,backref='user', lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+    #hashes password
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    #check password against hash
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
